9efe726c44d27b01833bce5795593ac2f1f92004
Lucien Tchandama <97041311+ktchandama@users.noreply.github.com>
2023-04-23 21:53:52 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+# Importing the necessary libraries
+import openai
+import gradio as gr
+
+# OpenAI API key goes here
+openai.api_key = "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"
+
+# Define the initial message
+messages = [
+    {"role": "system", "content": "You are a helpful and kind AI Assistant."},
+]
+
+# Define the chat function for handling user input and generating responses 
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+# Gradio interface
+inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
+outputs = gr.outputs.Textbox(label="Reply")
+
+gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="AI Chatbot",
+             description="Ask anything you want",
+             theme="compact").launch(share=True)
